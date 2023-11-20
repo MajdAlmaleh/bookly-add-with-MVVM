fe3d5e697bc39f4e437be6bc14953ab572cf5704
Majd Al-Maleh <majdalmaleh40@gmail.com>
2023-11-20 07:36:48 +0300

    dependency injectin modified

--- lib/core/utils/service_locator.dart
+++ lib/core/utils/service_locator.dart
@@ -1,15 +1,12 @@
-import 'package:bookly/Features/home/data/repos/home_repo_impl.dart';
-import 'package:dio/dio.dart';
 import 'package:get_it/get_it.dart';
+import 'package:bookly/core/utils/api_service.dart';
+import 'package:bookly/features/home/data/repos/home_repo_impl.dart';
+import 'package:dio/dio.dart';
 
-import 'api_service.dart';
-
-final getIt = GetIt.instance;
+GetIt getIt = GetIt.instance;
 
 void setupServiceLocator() {
-  getIt.registerSingleton<ApiService>(ApiService(Dio()));
-  getIt.registerSingleton<HomeRepoImpl>(HomeRepoImpl(
-    getIt.get<ApiService>(),
-  ));
+  getIt.registerLazySingleton<Dio>(() => Dio());
+  getIt.registerLazySingleton<ApiService>(() => ApiService(getIt<Dio>()));
+  getIt.registerLazySingleton<HomeRepoImpl>(() => HomeRepoImpl(getIt<ApiService>()));
 }
-
--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,14 @@
 import 'package:bookly/constants.dart';
-import 'package:bookly/core/utils/api_service.dart';
 import 'package:bookly/core/utils/app_router.dart';
 import 'package:bookly/core/utils/service_locator.dart';
 import 'package:bookly/features/home/data/repos/home_repo_impl.dart';
 import 'package:bookly/features/home/presentation/manager/featured_books_cubit/featured_books_cubit.dart';
 import 'package:bookly/features/home/presentation/manager/newest_books_cubit/newest_books_cubit.dart';
-import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-void main() async {
+void main() {
   setupServiceLocator();
   runApp(const Bookly());
 }
